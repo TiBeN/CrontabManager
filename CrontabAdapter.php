241952ad3d88080179e1a789f14b539595a8edbf
TiBeN <legendre.benjamin@gmail.com>
2014-01-11 16:27:26 +0100

    initial project commit

---
+++ CrontabAdapter.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Can retrieve and write crontab data using the "crontab" command line.
+ * @author francepronet
+ *
+ */
+class CrontabAdapter {
+	
+	private $userName;
+	
+	private $useSudo;
+	
+	/**
+	 * Instantiate an crontabAdapter
+	 * @param String $userName Optional Specifie the crontab user where the adapter will try to read (by default user = runtime user)
+	 * @param Boolean $useSudo Tell Adapter use sudo command to connect to crontab of another user than the runtime user.
+	 * 	
+	 * About sudo :
+	 * If you want to work with the crontab of another user of the runtime user, you can allowing the runtime user to edit 
+	 * another user crontab by adding this kind of line in your sudoers using 'visudo'
+	 * For example : user www-data want to edit waylon crontab:
+	 * 
+	 * www-data        ALL=(waylon) NOPASSWD: /usr/bin/crontab 
+	 * 
+	 * Will tell sudo user www-data can execute /usr/bin/crontab as user waylon without typing any password (required in our case) 
+	 */
+	public function __construct($userName = null, $useSudo = false) {
+		if($userName) $this->userName = $userName;
+		$this->useSudo = $useSudo;
+	}
+	
+	/**
+	 * Read the crontab and return 
+	 * raw data
+	 * @return String $output the crontab raw data 
+	 */	
+	public function readCrontab() {
+			
+		$crontabCommandLine = (isset($this->userName) && $this->useSudo) 
+			? sprintf('sudo -n -u %s crontab -l', $this->userName)
+			: ($this->userName ? sprintf('crontab -u %s -l', $this->userName) : 'crontab -l') 
+		;
+			
+		exec($crontabCommandLine . ' 2>&1', $output, $exitCode);
+				
+		/* exec error handling */
+		if($exitCode !== 0) {
+			
+			/* Special case : the crontab is empty throw bad exit code but access is ok */
+			if(!preg_match('/^no crontab for .+$/', $output[0])) {
+				throw new DomainException('Error when trying to read crontab : ' . implode(' ', $output));
+			}
+			else {
+				$output = '';
+			}				
+		}
+		else {
+			$output = implode("\n", $output);
+		}
+		
+		return $output;
+		
+	}
+	
+	/**
+	 * Write the raw crontab data to the crontab.
+	 * @param String $crontabRawData
+	 */
+	public function writeCrontab($crontabRawData) {
+		
+		$crontabRawData = escapeshellarg($crontabRawData);
+		
+		$crontabCommandLine = (isset($this->userName) && $this->useSudo)
+			? sprintf('echo %s | sudo -n -u %s crontab -',  $crontabRawData, $this->userName)
+			: ($this->userName 
+				? sprintf('echo %s | crontab -u %s -', $crontabRawData, $this->userName) 
+				: sprintf('echo %s | crontab -', $crontabRawData)
+			)
+		;
+		
+		exec($crontabCommandLine . ' 2>&1', $output, $exitCode);		
+		
+		/* exec error handling */
+		if($exitCode !== 0) {
+				
+			/* Special case : the crontab is empty throw bad exit code but access is ok */
+			if(!preg_match('/^no crontab for .+$/', $output[0])) {
+				throw new DomainException('Error when trying to write crontab : ' . implode(' ', $output));
+			}
+			else {
+				$output = '';
+			}
+		}		
+		
+	}
+	
+}
---
+++ test/CrontabJobTest.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * CrontabJob class PHPUnit test cases
+ * @author FranceProNet
+ */
+require_once dirname(__FILE__) . '/../../CrontabManager/CrontabJob.php';
+	
+class CrontabJobTest extends PHPUnit_Framework_TestCase {
+	
+	protected function setUp() {
+
+	}	
+	
+	/**
+	 * Test parsing crontab and creation of cronjob 
+	 * @dataProvider crontabLines
+	 */
+	public function testCreateACrontabJobFromCrontabLine($crontabLine, $crontabJob) {
+		
+		$this->assertEquals($crontabJob, CrontabJob::createFromCrontabLine($crontabLine));
+		
+	}	
+	
+    public static function crontabLines()  {
+    	
+    	$crontabLines = array();
+    	
+    	$crontabJob = new CrontabJob();
+    	$crontabJob->minutes = '30';
+    	$crontabJob->hours = '23';
+    	$crontabJob->dayOfMonth = '*';
+    	$crontabJob->months = '*';
+    	$crontabJob->dayOfWeek = '*';
+    	$crontabJob->taskCommandLine = 'df >> /tmp/df.log';
+    	
+    	$crontabLines[] = array('30 23 * * * df >> /tmp/df.log', $crontabJob);
+
+    	
+    	$crontabJob = new CrontabJob();
+    	$crontabJob->minutes = '12';
+    	$crontabJob->hours = '10';
+    	$crontabJob->dayOfMonth = '2-5';
+    	$crontabJob->months = '*';
+    	$crontabJob->dayOfWeek = '*';
+    	$crontabJob->taskCommandLine = 'df >> /tmp/df.log';
+    	
+    	$crontabLines[] = array('12 10 2-5 * * df >> /tmp/df.log', $crontabJob);
+
+    	
+    	$crontabJob = new CrontabJob();
+    	$crontabJob->minutes = '59';
+    	$crontabJob->hours = '23';
+    	$crontabJob->dayOfMonth = '*/2';
+    	$crontabJob->months = '*';
+    	$crontabJob->dayOfWeek = '*';
+    	$crontabJob->taskCommandLine = 'df >> /tmp/df.log';
+    	
+    	$crontabLines[] = array('59 23 */2 * * df >> /tmp/df.log', $crontabJob);
+    	
+    	
+    	$crontabJob = new CrontabJob();
+    	$crontabJob->minutes = '0';
+    	$crontabJob->hours = '0';
+    	$crontabJob->dayOfMonth = '25-31';
+    	$crontabJob->months = '1,3,5,7,8,10,12';
+    	$crontabJob->dayOfWeek = '0';
+    	$crontabJob->taskCommandLine = 'my-script.sh';
+    	
+    	$crontabLines[] = array('0 0 25-31 1,3,5,7,8,10,12 0 my-script.sh', $crontabJob);
+    	
+    	/* space at start test case */
+    	$crontabJob = new CrontabJob();
+    	$crontabJob->minutes = '0';
+    	$crontabJob->hours = '*';
+    	$crontabJob->dayOfMonth = '*/2';
+    	$crontabJob->months = '*';
+    	$crontabJob->dayOfWeek = '*';
+    	$crontabJob->taskCommandLine = 'my-script.sh';
+    	$crontabJob->comments = 'commentaire blablabla';
+    	
+    	$crontabLines[] = array(' 0 * */2 * * my-script.sh #commentaire blablabla', $crontabJob);
+
+    	/* Tab or spaces test case */
+    	$crontabJob = new CrontabJob();    	
+    	$crontabJob->minutes = '0';
+    	$crontabJob->hours = '*';
+    	$crontabJob->dayOfMonth = '*/2';
+    	$crontabJob->months = '*';
+    	$crontabJob->dayOfWeek = '*';
+    	$crontabJob->taskCommandLine = 'my-script.sh';
+    	$crontabJob->comments = 'commentaire blablabla';
+    	
+    	$crontabLines[] = array('0	*	 */2	  *		  *		 my-script.sh #commentaire blablabla', $crontabJob);
+    	
+    	/* Cron shortcut test case */
+    	$crontabJob = new CrontabJob();
+    	$crontabJob->shortCut = 'daily';
+    	$crontabJob->taskCommandLine = 'my-script.sh';
+    	
+    	$crontabLines[] = array('@daily my-script.sh', $crontabJob);    	
+    	
+    	return $crontabLines;
+
+    }
+    
+    public function testFormatCrontabLine() {
+
+    	/* Normal formated crontab line */
+    	$crontabJob = new CrontabJob();
+    	$crontabJob->minutes = '30';
+    	$crontabJob->hours = '23';
+    	$crontabJob->taskCommandLine = 'df >> /tmp/df.log';
+    	 
+    	$this->assertEquals('30 23 * * * df >> /tmp/df.log', $crontabJob->formatCrontabLine());
+    	
+    	$crontabJob = new CrontabJob();
+    	
+    	/* Shortcut formated crontab line */
+    	$crontab = new CrontabJob();
+    	$crontabJob->shortCut = 'reboot';
+    	$crontabJob->taskCommandLine = 'echo "youpi"';
+    	
+    	$this->assertEquals('@reboot echo "youpi"', $crontabJob->formatCrontabLine());
+    	
+    	/* crontab line with comments */
+    	$crontabJob = new CrontabJob();
+    	$crontabJob->minutes = '30';
+    	$crontabJob->hours = '23';
+    	$crontabJob->taskCommandLine = 'df >> /tmp/df.log';
+    	$crontabJob->comments = 'This job is commented';
+    	 
+    	$this->assertEquals('30 23 * * * df >> /tmp/df.log #This job is commented', $crontabJob->formatCrontabLine());    	
+    	
+    }
+    
+    /**
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Crontab line not well formated then can't be parsed
+     */
+	public function testCrontabLineNotWellFormatedNotEnoughStars() {
+		CrontabJob::createFromCrontabLine('* * 15 * youpi');		
+	}    
+
+	/**
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Crontab line not well formated then can't be parsed
+     */
+	public function testCrontabLineNotWellFormatedShortcutWithoutArobase() {
+		CrontabJob::createFromCrontabLine('daily youpi');		
+	}    
+
+	/**
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Crontab line not well formated then can't be parsed
+     */
+	public function testCrontabLineNotWellFormatedUnknownShortcut() {
+		CrontabJob::createFromCrontabLine('@maintenant youpi');		
+	}    
+    
+    
+    /**
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage CrontabJob contain's no task command line
+     */
+    public function testNoTaskSetException()  {
+    	
+    	$crontabJob = new CrontabJob();
+    	$crontabJob->minutes = '30';
+    	$crontabJob->hours = '23';
+
+		$crontabJob->formatCrontabLine();    	
+    	
+    }
+	
+}		
+
+?>
